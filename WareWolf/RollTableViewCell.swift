738a99bab4c7d0de964ba61d7c88deedaa2242f7
FALCON <falcon.x.0328@gmail.com>
2017-11-20 15:04:48 +0900

    役職のセルを用意

---
+++ WareWolf/RollTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RollTableViewCell.swift
+//  WareWolf
+//
+//  Created by falcon@mac on H29/11/20.
+//  Copyright © 平成29年 NagaoLab. All rights reserved.
+//
+
+import UIKit
+
+class RollTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
