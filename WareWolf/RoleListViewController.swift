80961a35ca127631e8bba7a2cfbf8f9a0e4cce2e
FALCON <falcon.x.0328@gmail.com>
2017-11-20 14:57:53 +0900

    役職のセルを表示

--- WareWolf/RoleListViewController.swift
+++ WareWolf/RoleListViewController.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
-class RoleListViewController: UIViewController {
-
+class RoleListViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    private let ROLL_CELL = "ROLL_CELL"
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +32,16 @@ class RoleListViewController: UIViewController {
     
     @IBAction func tappedRoleListButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1時的に1行だけ
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.ROLL_CELL, for: indexPath)
+        return cell
     }
     
 }
