cd05ceb1645adfb4394589fa7882607e163ebc58
FALCON <falcon.x.0328@gmail.com>
2017-11-21 13:38:18 +0900

    表示する機能を実装

--- WareWolf/CheckViewController.swift
+++ WareWolf/CheckViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class CheckViewController: UIViewController {
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    var villager = 0
+    var wereWolf = 0
+    var fox = 0
     var roleList : [Int:Int] = [:]
     
     @IBOutlet weak var villagerLabel: UILabel!
@@ -20,12 +25,47 @@ class CheckViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        for (key , value) in self.roleList {
+            let role = self.appDelegate.roleManager.ID2Role(ID: key, roleList: self.appDelegate.roleList)
+            if role != nil {
+                if role!.side == .Villager {
+                    self.villager += value
+                }else if role!.side == .WereWolf {
+                    self.wereWolf += value
+                }else if role!.side == .Fox {
+                    self.fox += value
+                }
+            }
+        }
+        
+        self.villagerLabel.text = "市民サイド：" + self.showNumberOfRoleText(number: self.villager)
+        self.wereWolfLabel.text = "人狼サイド：" + self.showNumberOfRoleText(number: self.wereWolf)
+        self.foxLabel.text = "　狐サイド：" + self.showNumberOfRoleText(number: self.fox)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func tappedRoleButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /// 数字をいれると、「x名」
+    ///
+    /// - Parameter number: メンバー数
+    /// - Returns: ラベル表示用のテキスト
+    private func showNumberOfRoleText(number : Int) -> String{
+        if number < 10 {
+            return " \(number)名"
+        }else{
+            return "\(number)名"
+        }
+    }
 
     /*
     // MARK: - Navigation
