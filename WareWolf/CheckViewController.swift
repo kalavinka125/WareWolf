c1642939f8da044a18adfd2009dc354260b18fbe
FALCON <falcon.x.0328@gmail.com>
2017-11-21 13:20:07 +0900

    ゲーム前確認画面の準備

---
+++ WareWolf/CheckViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CheckViewController.swift
+//  WareWolf
+//
+//  Created by falcon@mac on H29/11/21.
+//  Copyright © 平成29年 NagaoLab. All rights reserved.
+//
+
+import UIKit
+
+class CheckViewController: UIViewController {
+    var roleList : [Int:Int] = [:]
+    
+    @IBOutlet weak var villagerLabel: UILabel!
+    @IBOutlet weak var wereWolfLabel: UILabel!
+    @IBOutlet weak var foxLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
