575025cadbc510b495741c1e5b650f2b24ab3fdc
FALCON <falcon.x.0328@gmail.com>
2017-11-21 14:55:49 +0900

    プレイヤー名を出す処理を実装

--- WareWolf/RoleCheckViewController.swift
+++ WareWolf/RoleCheckViewController.swift
@@ -11,9 +11,17 @@ import UIKit
 class RoleCheckViewController: UIViewController {
     private let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.nameLabel.adjustsFontSizeToFitWidth = true
+        self.nameLabel.text = self.appDelegate.playerList[self.appDelegate.playerID].name
     }
 
     override func didReceiveMemoryWarning() {
