712555efcb562f1036753a16b2afe3c3e979ba6e
FALCON <falcon.x.0328@gmail.com>
2017-11-29 21:13:57 +0900

    議論中の役職確認用画面の用意

---
+++ WareWolf/RoleCheckInDiscussionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RoleCheckInDiscussionViewController.swift
+//  WareWolf
+//
+//  Created by falcon@mac on H29/11/29.
+//  Copyright © 平成29年 NagaoLab. All rights reserved.
+//
+
+import UIKit
+
+class RoleCheckInDiscussionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
