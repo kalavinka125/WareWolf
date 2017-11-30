3c5ca2f97cac15a28dd6fa51ecfa2efd7498b0d5
FALCON <falcon.x.0328@gmail.com>
2017-12-01 00:57:40 +0900

    新しいセルの追加

---
+++ WareWolf/RoleHintTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RoleHintTableViewCell.swift
+//  WareWolf
+//
+//  Created by falcon@mac on H29/12/01.
+//  Copyright © 平成29年 NagaoLab. All rights reserved.
+//
+
+import UIKit
+
+class RoleHintTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
